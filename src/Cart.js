fab02fe0dbdf744ed4c9af21c9ee70ed1b40cab1
brawny271 <116960735+brawny271@users.noreply.github.com>
2023-10-19 07:22:48 +0530

    Update Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -1,30 +1,45 @@
+
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart } from './Redux/action';
+import { removeFromCart, increaseQuantity, decreaseQuantity } from './actions';
 
-const Cart = () => {
+function Cart() {
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   return (
-    <div className="cart">
+    <div>
       <h2>Shopping Cart</h2>
-      {cart.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
           {cart.map((item) => (
-            <li key={item.id}>
-              <img src={item.image} alt={item.title} />
-              <p>{item.title}</p>
-              <p>${item.price}</p>
-              <button onClick={() => dispatch(removeFromCart(item))}>Remove</button>
-            </li>
+            <tr key={item.id}>
+              <td>{item.title}</td>
+              <td>${item.price}</td>
+              <td>
+                <button onClick={() => dispatch(decreaseQuantity(item.id)}>-</button>
+                {item.quantity}
+                <button onClick={() => dispatch(increaseQuantity(item.id))}>+</button>
+              </td>
+              <td>${item.totalPrice}</td>
+              <td>
+                <button onClick={() => dispatch(removeFromCart(item.id)}>Remove</button>
+              </td>
+            </tr>
           ))}
-        </ul>
-      )}
+        </tbody>
+      </table>
     </div>
   );
-};
+}
 
 export default Cart;
